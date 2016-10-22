a20d65425299fd6c9b7f60f1ec36fa91c5bf75bb
Laurent Van Begin <laurent.vanbegin76@gmail.com>
2016-10-22 14:49:13 +0200

    INT: improve message.

--- src/socket.cpp
+++ src/socket.cpp
@@ -29,24 +29,24 @@ void Socket::establishConnection(void) {
 		accept();
 		break;
 	default:
-		throw std::runtime_error("unexpected case.");
+		throw std::runtime_error("Socket: unexpected case.");
 	}
 }
 
 void Socket::connectHost(void) {
 	fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (-1 == fd)
-		throw std::runtime_error("proxyClient: socket creation failed");
+		throw std::runtime_error("Socket: socket creation failed");
 	sockaddr_in sin;
 	hostent *he = gethostbyname(host.c_str());
 	if (nullptr == he)
-		throw std::runtime_error("proxyClient: cannot convert hostname");
+		throw std::runtime_error("Socket: cannot convert hostname");
 	memcpy(&sin.sin_addr.s_addr, he->h_addr, he->h_length);
 	printf(" %d %d %d %d\n", ((char *)&sin.sin_addr.s_addr)[0], ((char *)&sin.sin_addr.s_addr)[1], ((char *)&sin.sin_addr.s_addr)[2], ((char *)&sin.sin_addr.s_addr)[3]);
 	sin.sin_family = AF_INET;
 	sin.sin_port = htons(port);
 	if (-1 == connect(fd, reinterpret_cast<sockaddr*>(&sin), sizeof(sin)))
-		throw std::runtime_error("proxyClient: cannot connect");
+		throw std::runtime_error("Socket: cannot connect");
 }
 
 void Socket::accept(void) {
@@ -71,7 +71,7 @@ void Socket::writeBytes(const void *buffer, size_t count) {
 	for (size_t nbTotalWritten = 0; nbTotalWritten < count; ) {
 		const ssize_t nbWritten = write(fd, ptr + nbTotalWritten, count - nbTotalWritten);
 		if (-1 == nbWritten && !(EINTR == errno))
-			throw std::runtime_error("send bytes to proxy server failed");
+			throw std::runtime_error("Socket: send bytes failed");
 		nbTotalWritten += nbWritten;
 	}
 }
@@ -81,7 +81,7 @@ void Socket::readBytes(void *buffer, size_t count) {
 	for (size_t nbTotalRead = 0; nbTotalRead < count; ) {
 		const ssize_t nbRead = read(fd, ptr + nbTotalRead, count - nbTotalRead);
 		if (0 >= nbRead)
-			throw std::runtime_error("send bytes to proxy server failed");
+			throw std::runtime_error("Socket: read bytes failed");
 		nbTotalRead += nbRead;
 	}
 }
@@ -91,7 +91,7 @@ static int getConnection(int sockfd) {
 	socklen_t client_len = sizeof(client_addr);
 	const int newsockfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_len);
 	if (-1 == newsockfd)
-		throw std::runtime_error("proxyServer: accept failed");
+		throw std::runtime_error("Socket: accept failed");
 	close(sockfd);
 	return newsockfd;
 }
@@ -100,16 +100,16 @@ static int serverSocket(uint16_t port) {
 	struct sockaddr_in serv_addr { };
 	const int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (-1 == sockfd)
-		throw std::runtime_error("proxyServer: cannot create socket");
+		throw std::runtime_error("Socket: cannot create socket");
 	int enable = 1;
 	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
-		throw std::runtime_error("proxyServer: setsockopt(SO_REUSEADDR) failed");
+		throw std::runtime_error("Socket: setsockopt(SO_REUSEADDR) failed");
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
 	serv_addr.sin_port = htons(port);
 	if (bind(sockfd, reinterpret_cast<struct sockaddr *> (&serv_addr), sizeof(serv_addr)) < 0)
-		throw std::runtime_error("proxyServer: cannot bind socker");
+		throw std::runtime_error("Socket: cannot bind socker");
 	if (-1 == listen(sockfd,5))
-		throw std::runtime_error("proxyServer: cannot listen");
+		throw std::runtime_error("Socket: cannot listen");
 	return sockfd;
 }
