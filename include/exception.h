322ef0b4f7be55b027efabd6eaad180dd4fa9803
Laurent Van Begin <laurent.vanbegin76@gmail.com>
2016-11-23 18:50:28 +0100

    INT: add missing include

--- include/exception.h
+++ include/exception.h
@@ -30,6 +30,9 @@
 #ifndef EXCEPTION_H__
 #define EXCEPTION_H__
 
+#include <string>
+#include <stdexcept>
+
 #define THROW(E, message) throw E(std::string(__func__) + std::string(": ") + std::string(message))
 
 #endif
