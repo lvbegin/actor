5bba9ddaa26c5d34a93c1d8492b21283ffda509f
Laurent Van Begin <laurent.vanbegin76@gmail.com>
2017-07-24 12:17:32 +0200

    INT: add class to allow injecting pre/post command processing.

---
+++ src/commandExecutor.cpp
@@ -0,0 +1,47 @@
+/* Copyright 2017 Laurent Van Begin
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE OpenSSL PROJECT ``AS IS'' AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OpenSSL PROJECT OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <commandExecutor.h>
+
+const PreCommandHook DEFAULT_PRECOMMAND_HOOK = [](Command, const RawData &, const ActorLink &) { return StatusCode::OK; };
+const PostCommandHook DEFAULT_POSTCOMMAND_HOOK = [](Command, const RawData &, const ActorLink &) { };
+
+CommandExecutor::CommandExecutor(const commandMap map[], size_t size) :
+	CommandExecutor(DEFAULT_PRECOMMAND_HOOK, DEFAULT_POSTCOMMAND_HOOK, map, size) { }
+CommandExecutor::CommandExecutor(PreCommandHook preCommand, PostCommandHook postCommand, const commandMap map[], size_t size) :
+	preCommand(preCommand), postCommand(postCommand), actorCommand(ActorCommand(map, size)) { }
+CommandExecutor::~CommandExecutor() = default;
+
+StatusCode CommandExecutor::execute(Command commandCode, const RawData &data, const ActorLink &actorLink) const {
+	auto rc = preCommand(commandCode, data, actorLink);
+	if (StatusCode::OK != rc)
+		return rc;
+	rc  = actorCommand.execute(commandCode, data, actorLink);
+	return (postCommand(commandCode, data, actorLink), rc);
+}
